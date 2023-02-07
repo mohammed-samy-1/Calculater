db3dee1d0b819935865eafe99af8b0ce54043d3f
mo samy <medo.samy.0123@mail.com>
2023-02-07 15:18:37 +0200

    simple Calculater

--- app/src/main/java/com/example/calculater/MainActivity.java
+++ app/src/main/java/com/example/calculater/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initViews();
         initAction();
-
+        startActivityForResult(new Intent(this , MainActivity.class),555);
     }
 
     private void initAction() {
