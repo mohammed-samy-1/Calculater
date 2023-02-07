70181567d75789e6de55f2bcf3299b89b0842b9f
mo samy <medo.samy.0123@mail.com>
2023-02-07 18:13:27 +0200

    simple Calculator

--- app/src/main/java/com/example/calculater/MainActivity.java
+++ app/src/main/java/com/example/calculater/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initViews();
         initAction();
-        startActivityForResult(new Intent(this , MainActivity.class),555);
     }
 
     private void initAction() {
